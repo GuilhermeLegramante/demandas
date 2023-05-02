2ece913b15cec56ff0719e15935e82e6c8c8080c
Guilherme <guilhermelegramante@gmail.com>
2023-05-02 11:41:24 -0300

    .

---
+++ app/Repositories/ClientRepository.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Services\LogService;
+use Illuminate\Support\Facades\DB;
+
+class ClientRepository
+{
+    private $table = 'clients';
+
+    private $baseQuery;
+
+    public function __construct()
+    {
+        $this->baseQuery = DB::table($this->table)
+            ->select(
+                $this->table . '.id AS id',
+                $this->table . '.name AS name',
+                $this->table . '.email AS email',
+                $this->table . '.phone AS phone',
+                $this->table . '.created_at AS createdAt',
+                $this->table . '.updated_at AS updatedAt',
+            );
+    }
+
+    public function all(string $search = null, string $sortBy = 'id', string $sortDirection = 'asc', string $perPage = '30')
+    {
+        return $this->baseQuery
+            ->where([
+                [$this->table . '.id', 'like', '%' . $search . '%'],
+            ])
+            ->orWhere([
+                [$this->table . '.name', 'like', '%' . $search . '%'],
+            ])
+            ->orderBy($sortBy, $sortDirection)
+            ->paginate($perPage);
+    }
+
+    public function allSimplified()
+    {
+        return $this->baseQuery->get();
+    }
+
+    public function save($data)
+    {
+        LogService::saveLog(
+            session()->get('userId'),
+            $this->table,
+            'I',
+            date('Y-m-d H:i:s'),
+            json_encode($data),
+            null
+        );
+
+        return DB::table($this->table)
+            ->insertGetId(
+                [
+                    'name' => $data['name'],
+                    'email' => isset($data['email']) ? $data['email'] : null,
+                    'phone' => isset($data['phone']) ? $data['phone'] : null,
+                    'user_id' => session()->get('userId'),
+                    'created_at' => now(),
+                ]
+            );
+    }
+
+    public function update($data)
+    {
+        $oldData = $this->findById($data['recordId']);
+
+        LogService::saveLog(
+            session()->get('userId'),
+            $this->table,
+            'U',
+            date('Y-m-d H:i:s'),
+            json_encode($oldData),
+            json_encode($data)
+        );
+
+        DB::table($this->table)
+            ->where('id', $data['recordId'])
+            ->update(
+                [
+                    'name' => $data['name'],
+                    'email' => isset($data['email']) ? $data['email'] : null,
+                    'phone' => isset($data['phone']) ? $data['phone'] : null,
+                    'user_id' => session()->get('userId'),
+                    'updated_at' => now(),
+                ]
+            );
+    }
+
+    public function delete($data)
+    {
+        $oldData = $this->findById($data['recordId']);
+
+        LogService::saveLog(
+            session()->get('userId'),
+            $this->table,
+            'D',
+            date('Y-m-d H:i:s'),
+            json_encode($oldData),
+            null
+        );
+
+        DB::table($this->table)
+            ->where('id', $data['recordId'])
+            ->delete();
+    }
+
+    public function findById($id)
+    {
+        return $this->baseQuery
+            ->where($this->table . '.id', $id)
+            ->get()
+            ->first();
+    }
+}
