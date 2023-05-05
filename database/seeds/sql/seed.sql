1b056d0092c51a6d92ce03f2fae8bc39a3a17e76
Guilherme <guilhermelegramante@gmail.com>
2023-05-05 15:01:25 -0300

    .

--- database/seeds/sql/seed.sql
+++ database/seeds/sql/seed.sql
@@ -1,1 +1,2 @@
 insert into `users`(`login`,`name`,`password`,`email`,`is_admin`, `created_at`) values ('GUILHERME','GUILHERME MARTINS','ffa5d4e38dfb646691f71683f08ebc7e8e26eb62','guilhermelegrmante@gmail.com',1, '2023-05-01 08:58:32');
+insert into `users`(`login`,`name`,`password`,`email`,`is_admin`, `created_at`) values ('PABLO','PABLO VENZON','ffa5d4e38dfb646691f71683f08ebc7e8e26eb62','pablovenzon@hotmail.com',1, '2023-05-01 08:58:32');
--- resources/views/partials/login-logo-and-text.blade.php
+++ resources/views/partials/login-logo-and-text.blade.php
@@ -1,5 +1,5 @@
 <div class="login-logo">
-    <img style="width:30%; padding: 4%;" src="vendor/adminlte/dist/img/logo.png" alt=""><br>
+    <img style="width:30%; padding: 4%;" src="https://hardsoft.s3.sa-east-1.amazonaws.com/_lsmarketing/assets/logo.png" alt=""><br>
     {{-- <a style="font-weight: 350;" href="{{ route('loginView') }}"><strong>Marca & Sinal</strong></a> --}}
     <h1><strong>LS Marketing</strong></h1>
 </div>
